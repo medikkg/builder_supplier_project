079eb0b05954e45429dcf6193fc39e417e871b8b
medikkg <leokalai@gmail.com>
2023-08-28 18:06:41 +0600

    Коммит

---
+++ pyQT/main.py
@@ -0,0 +1,41 @@
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("Простая программа")
+        self.setGeometry(300, 250, 350, 200)
+
+        self.new_text = QtWidgets.QLabel(self)
+
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Это базовая надпись")
+        self.main_text.move(100, 100)
+        self.main_text.adjustSize()
+
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(70, 150)
+        self.btn.setText('Нажми на меня')
+        self.btn.setFixedWidth(200)
+        self.btn.clicked.connect(self.add_label)
+
+
+    def add_label(self):
+        self.new_text.setText("Вторая надпись")
+        self.new_text.move(100, 50)
+        self.new_text.adjustSize()
+def application():
+    app = QApplication(sys.argv)
+    window = Window()
+
+    window.show()
+    sys.exit(app.exec())
+
+
+
+if __name__ == '__main__':
+    application()
